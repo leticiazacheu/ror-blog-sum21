7f26eb800e7c788e3eb2559fc28225ae3786df32
Leticia Fernandes <leticiatotaa@gmail.com>
2021-06-30 23:48:28 -0600

    components done

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :title, :category, presence: true
 end
---
+++ client/src/components/posts/PostList.js
@@ -0,0 +1,11 @@
+import Post from './Post';
+
+const PostList = ({ blogId, posts, updatePost, deletePost }) => (
+  <>
+    { posts.map( p => 
+      <Post {...p} updatePost={updatePost} deletePost={deletePost} />  
+    )}
+  </>
+)
+
+export default PostList;
